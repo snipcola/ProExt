8833922fe130ab77ec1f9f4f9023749f4b576778
Vytrol <vytrol.6@gmail.com>
2023-11-16 18:34:18 +0000

    feat(bomb_timer, bomb_esp): organize

--- src/cheat/features/bomb_timer.rs
+++ src/cheat/features/bomb_timer.rs
@@ -1,122 +1,28 @@
 use std::{sync::{Arc, Mutex}, time::{Instant, Duration}};
 use imgui::Ui;
 use lazy_static::lazy_static;
-use mint::{Vector4, Vector3};
-use crate::{utils::{config::{Config, CONFIG, WindowPosition}, process_manager::read_memory_auto}, ui::functions::color_u32_to_f32, cheat::classes::offsets::{BOMB_OFFSETS, PAWN_OFFSETS}};
+use mint::Vector4;
+use crate::{utils::config::{Config, CONFIG, WindowPosition}, ui::functions::color_u32_to_f32};
 
 lazy_static! {
     pub static ref IS_PLANTED: Arc<Mutex<bool>> = Arc::new(Mutex::new(false));
     pub static ref PLANT_TIME: Arc<Mutex<Option<Instant>>> = Arc::new(Mutex::new(None));
-
-    pub static ref BOMB_POSITION: Arc<Mutex<Option<Vector3<f32>>>> = Arc::new(Mutex::new(None));
-    pub static ref BOMB_SITE: Arc<Mutex<Option<String>>> = Arc::new(Mutex::new(None));
 }
 
-pub fn get_planted_bomb(bomb_address: u64) -> Option<u64> {
-    let mut planted_bomb: u64 = 0;
-
-    if !read_memory_auto(bomb_address, &mut planted_bomb) {
-        return None;
-    }
-
-    if !read_memory_auto(planted_bomb, &mut planted_bomb) {
-        return None;
-    }
-
-    return Some(planted_bomb);
-}
-
-pub fn get_bomb_planted(bomb_address: u64) -> bool {
-    let mut is_bomb_planted: bool = false;
-    
-    if !read_memory_auto(bomb_address - 0x8, &mut is_bomb_planted) {
-        return false;
-    };
-
-    return is_bomb_planted;
-}
-
-pub fn get_bomb_site(planted_bomb: u64) -> Option<String> {
-    let mut site: u32 = 0;
-
-    if !read_memory_auto(planted_bomb + (*BOMB_OFFSETS.lock().unwrap()).bomb_site as u64, &mut site) {
-        return None;
-    }
-
-    if site == 1 {
-        return Some("B".to_string());
-    } else {
-        return Some("A".to_string());
-    }
-}
-
-pub fn get_bomb_position(planted_bomb: u64) -> Option<Vector3<f32>> {
-    let mut bomb_node = 0;
-
-    if !read_memory_auto(planted_bomb + (*PAWN_OFFSETS.lock().unwrap()).game_scene_node as u64, &mut bomb_node) {
-        return None;
-    }
-
-    let mut bomb_pos = Vector3 { x: 0.0, y: 0.0, z: 0.0 };
-
-    if !read_memory_auto(bomb_node + (*PAWN_OFFSETS.lock().unwrap()).vec_abs_origin as u64, &mut bomb_pos) {
-        return None;
-    }
-
-    return Some(bomb_pos);
-}
-
-pub fn render_bomb_timer(ui: &mut Ui, enabled: bool, bomb_address: u64, config: Config) {
+pub fn render_bomb_timer(ui: &mut Ui, bomb_planted: bool, bomb_site: Option<String>, config: Config) {
     let window_position = config.window_positions.bomb_timer;
 
     let mut is_planted = IS_PLANTED.lock().unwrap();
     let mut plant_time = PLANT_TIME.lock().unwrap();
-    
-    let is_bomb_planted = get_bomb_planted(bomb_address);
-    let planted_bomb = match is_bomb_planted {
-        true => get_planted_bomb(bomb_address),
-        false => None
-    };
 
-    let bomb_site = match is_bomb_planted {
-        true => match planted_bomb {
-            Some(bomb) => get_bomb_site(bomb),
-            _ => None
-        },
-        false => None
-    };
-    
-    let bomb_pos = match is_bomb_planted {
-        true => match planted_bomb {
-            Some(bomb) => get_bomb_position(bomb),
-            _ => None
-        },
-        false => None
-    };
-
-    if is_bomb_planted && !*is_planted && ((*plant_time).is_none() || plant_time.unwrap().elapsed() > Duration::from_secs(60)) {
+    if bomb_planted && !*is_planted && ((*plant_time).is_none() || plant_time.unwrap().elapsed() > Duration::from_secs(60)) {
         *is_planted = true;
         *plant_time = Some(Instant::now());
-
-        if let Some(bomb_pos) = bomb_pos {
-            *BOMB_POSITION.lock().unwrap() = Some(bomb_pos);
-        }
-
-        if let Some(bomb_site) = bomb_site.clone() {
-            *BOMB_SITE.lock().unwrap() = Some(bomb_site);
-        }
     }
     
-    if *is_planted && !is_bomb_planted {
+    if *is_planted && !bomb_planted {
         *is_planted = false;
         *plant_time = None;
-
-        *BOMB_POSITION.lock().unwrap() = None;
-        *BOMB_SITE.lock().unwrap() = None;
-    }
-
-    if !enabled {
-        return;
     }
 
     let remaining_time: Option<u64> = {
