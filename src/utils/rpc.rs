bfb96ae18c853f0e2c69eaea6af2511abaf04aa7
Vytrol <vytrol.6@gmail.com>
2023-11-17 00:19:04 +0000

    fix(discord_rpc): apply hotfix

--- src/utils/rpc.rs
+++ src/utils/rpc.rs
@@ -8,20 +8,13 @@ lazy_static! {
     pub static ref RPC_CLIENT: Arc<Mutex<Client>> = Arc::new(Mutex::new(Client::new(*RPC_CLIENT_ID)));
 }
 
-fn current_time_u64() -> u64 {
-    let start = SystemTime::now();
-    let since_epoch = start.duration_since(UNIX_EPOCH).unwrap();
-    
-    return since_epoch.as_secs();
-}
-
-pub fn set_rpc_activity(client: &mut Client) {
+pub fn set_rpc_activity(client: &mut Client, started: u64) {
     client
         .set_activity(| activity | {
             activity
                 .state("An open-source, external CS2 cheat.")
                 .assets(| assets | assets.large_image("cs2"))
-                .timestamps(| timestamps | timestamps.start(current_time_u64()))
+                .timestamps(| timestamps | timestamps.start(started))
         })
         .ok();
 }
@@ -34,7 +27,7 @@ pub fn clear_rpc_activity(client: &mut Client) {
 
 pub fn initialize_rpc() {
     let rpc_client = RPC_CLIENT.clone();
-    let mut rpc_enabled = false;
+    let started = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs();
     
     thread::spawn(move || {
         let mut client = rpc_client.lock().unwrap();
@@ -44,19 +37,9 @@ pub fn initialize_rpc() {
             let config = CONFIG.lock().unwrap().clone();
 
             if config.misc.enabled && config.misc.discord_rpc_enabled {
-                if !rpc_enabled {
-                    set_rpc_activity(&mut client);
-                    
-                    rpc_enabled = true;
-                    (*CONFIG.lock().unwrap()).misc.discord_rpc_enabled = true;
-                }
+                set_rpc_activity(&mut client, started);
             } else {
-                if rpc_enabled {
-                    clear_rpc_activity(&mut client);
-                    
-                    rpc_enabled = false;
-                    (*CONFIG.lock().unwrap()).misc.discord_rpc_enabled = false;
-                }
+                clear_rpc_activity(&mut client);
             }
         }
     });
