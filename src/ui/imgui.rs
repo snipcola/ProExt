9457368ee5cab821b42771ce26ddfb7dbe9df6e8
Vytrol <vytrol.6@gmail.com>
2023-12-03 07:03:48 +0000

    fix: scaling issues

--- src/ui/imgui.rs
+++ src/ui/imgui.rs
@@ -6,6 +6,12 @@ use imgui::{Context, FontSource, FontConfig, FontGlyphRanges};
 use imgui_winit_support::{WinitPlatform, HiDpiMode};
 use crate::{ui::windows::Window, utils::config::ProgramConfig};
 
+pub fn add_imgui_default_font(fonts: &mut Vec<FontSource>, font_size: f32) {
+    fonts.push(FontSource::DefaultFontData {
+        config: Some(FontConfig { size_pixels: font_size, oversample_h: 3, oversample_v: 3, ..Default::default() })
+    });
+}
+
 pub fn add_imgui_font(fonts: &mut Vec<FontSource>, font_path: &str, font_size: f32, range: FontGlyphRanges) {
     let buffer = match read(font_path) {
         Ok(file) => file,
@@ -27,10 +33,12 @@ pub fn init_imgui(window: &Window) -> (WinitPlatform, Context) {
     imgui_context.set_log_filename(None);
 
     let mut winit_platform = WinitPlatform::init(&mut imgui_context);
-    winit_platform.attach_window(imgui_context.io_mut(), window.window(), HiDpiMode::Rounded);
+    winit_platform.attach_window(imgui_context.io_mut(), window.window(), HiDpiMode::Locked(1.0));
 
     let font_size = ProgramConfig::Imgui::FontSize;
-    let mut fonts = vec![FontSource::DefaultFontData { config: Some(FontConfig { size_pixels: font_size, oversample_h: 3, oversample_v: 3, ..Default::default() }) }];
+    let mut fonts = vec![];
+
+    add_imgui_default_font(&mut fonts, font_size);
     add_imgui_font(&mut fonts, ProgramConfig::Imgui::FontPaths::Chinese, font_size, FontGlyphRanges::chinese_full());
     add_imgui_font(&mut fonts, ProgramConfig::Imgui::FontPaths::Cryillic, font_size, FontGlyphRanges::cyrillic());
     add_imgui_font(&mut fonts, ProgramConfig::Imgui::FontPaths::Arabic, font_size, FontGlyphRanges::from_slice(&[0x600, 0x6FF, 0]));
